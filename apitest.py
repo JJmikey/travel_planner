96fe378c78515d36b606d538d9a2d096f5b979a3
JJmikey <144325604+JJmikey@users.noreply.github.com>
2024-01-23 17:21:32 +0800

    Update apitest.py

--- apitest.py
+++ apitest.py
@@ -10,6 +10,7 @@ Original file is located at
 
 from flask import Flask, jsonify, request
 #from flask_ngrok import run_with_ngrok
+import os
 
 app = Flask(__name__)
 #run_with_ngrok(app)  # Start ngrok when app is run
